a00de04cbf81d870786fe86167f21333a1b259d4
Erik <eas388@cornell.edu>
2023-03-28 04:52:15 -0800

    Moved filmstrips to actor folder, created json for animations

---
+++ asset/actor/gale_walk_large.tsx
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="gale_walk_large" tilewidth="253" tileheight="377" tilecount="15" columns="5">
+ <editorsettings>
+  <export target="gale_walk_large.json" format="json"/>
+ </editorsettings>
+ <image source="gale_filmstrip_large.png" width="1265" height="1131"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="35"/>
+   <frame tileid="1" duration="35"/>
+   <frame tileid="2" duration="35"/>
+   <frame tileid="3" duration="35"/>
+   <frame tileid="4" duration="35"/>
+   <frame tileid="5" duration="35"/>
+   <frame tileid="6" duration="35"/>
+   <frame tileid="7" duration="35"/>
+   <frame tileid="8" duration="35"/>
+   <frame tileid="9" duration="35"/>
+   <frame tileid="10" duration="35"/>
+   <frame tileid="11" duration="35"/>
+   <frame tileid="12" duration="35"/>
+   <frame tileid="13" duration="35"/>
+   <frame tileid="14" duration="35"/>
+  </animation>
+ </tile>
+</tileset>
